c219c71efd1016594c169f0b13d64007674eca75
sayyyho <323psh@naver.com>
2024-09-19 01:53:05 +0000

    배포

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return <div>멋사 축사 배포</div>;
+}
+
+export default App;
