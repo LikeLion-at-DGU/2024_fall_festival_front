15961d37a5b3aab5f3e8599ba702669594000d79
nicerjs23 <nicerjs23@gmail.com>
2024-09-20 15:55:45 +0900

    [Feature] #11 - 공연슬라이드 파일추가

--- src/pages/performance/PerformancePage.jsx
+++ src/pages/performance/PerformancePage.jsx
@@ -1,6 +1,6 @@
 //공연페이지 - 이쪽에서 각자 만든거 합치면 될거같아요
 import * as S from "./Styled";
-import { PerformanceSlide } from "./performanceslide/PerformanceSlide";
+import { PerformanceSlide } from "./components/performanceslide/PerformanceSlide";
 
 export const PerformancePage = () => {
   return (
