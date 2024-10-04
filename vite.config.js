4898de520a51b39cf6b28174f3543691f3963983
taejun0 <xownswns@naver.com>
2024-10-05 04:48:56 +0900

    feature: 카카오맵 현위치 기능 추가

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from "vite";
-import fs from 'fs';
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
-    },
-  },
   resolve: {
     alias: {
       "@atoms": path.resolve(__dirname, "src/atoms"),
