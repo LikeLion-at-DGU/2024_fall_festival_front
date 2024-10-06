9e750b7b41630f3258c815ec567c9781cf41e1a6
DongGun Lee <124961407+nicerjs23@users.noreply.github.com>
2024-10-06 13:27:33 +0900

    [Feature] #108 - 에러페이지 구현 (#109)

--- src/router.jsx
+++ src/router.jsx
@@ -4,6 +4,8 @@ import { DefaultLayout } from "@layout/DefaultLayout";
 import { BoothPage } from "@pages/booth/BoothPage";
 import { AboutPage } from "@pages/about/AboutPage";
 import { MainPage } from "@pages/main/MainPage";
+import { NotFound } from "@pages/notfound/NotFound";
+
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -19,6 +21,11 @@ export const router = createBrowserRouter([
       },
 
       { path: "/about", element: <AboutPage /> },
+      //에러페이지
+      {
+        path: "/error",
+        element: <NotFound />,
+      },
     ],
   },
 ]);
